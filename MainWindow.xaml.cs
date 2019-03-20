67850c271004d47ad4ec62379372f03c672450b5
IanMcT <ian.mctavish@tldsb.on.ca>
2019-03-20 10:05:44 -0400

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace drawingExample
+namespace fileWriteExample
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,13 +23,69 @@ namespace drawingExample
         public MainWindow()
         {
             InitializeComponent();
-            Rectangle r = new Rectangle();
-            r.Width = 100;
-            r.Height = 100;
-            r.Fill = Brushes.Red;
-            canvas.Children.Add(r);
-            Canvas.SetLeft(r, 10);
-            Canvas.SetTop(r, 20);
+        }
+
+        private void btnWrite_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamWriter sw =
+                    new System.IO.StreamWriter(txtFileName.Text);
+                sw.WriteLine(txtInput.Text);
+                sw.Flush();
+                sw.Close();
+            }//end try
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }//end catch
+        }
+
+        private void btnAppend_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamWriter sw =
+                    new System.IO.StreamWriter(txtFileName.Text, true);
+                sw.WriteLine(txtInput.Text);
+                
+                sw.Flush();
+                sw.Close();
+            }//end try
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }//end catch
+        }
+
+        private void btnSquare_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamWriter sw =
+                    new System.IO.StreamWriter(txtFileName.Text);
+                //sw.WriteLine(txtInput.Text);
+                //get number
+                int theNumber;
+                int.TryParse(txtInput.Text, out theNumber);
+                //loop from 1 to the number
+                for (int i = 1; i <= theNumber; i++)
+                {
+                    //list the numbers 1 to the number
+                    //after each number add a tab
+                    //after each tab add the square
+                    sw.WriteLine(i.ToString() 
+                        + "\t" 
+                        + (i*i).ToString());
+                }
+
+                sw.Flush();
+                sw.Close();
+            }//end try
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }//end catch
         }
     }
 }
