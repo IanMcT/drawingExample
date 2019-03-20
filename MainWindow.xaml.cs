0aeda3493699f5d98478b92434b1e5a1933d4ec3
IanMcT <ian.mctavish@tldsb.on.ca>
2019-03-20 05:59:08 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace drawingExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Rectangle r = new Rectangle();
+            r.Width = 100;
+            r.Height = 100;
+            r.Fill = Brushes.Red;
+            canvas.Children.Add(r);
+            Canvas.SetLeft(r, 10);
+            Canvas.SetTop(r, 20);
+        }
+    }
+}
